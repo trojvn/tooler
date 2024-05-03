17bdefd2e5345ba72a3a645d270fb2e29930f621
trojvn <mrx.trojan@gmail.com>
2024-05-03 15:03:27 +0300

    kill by name

--- tooler/processes.py
+++ tooler/processes.py
@@ -1,17 +1,21 @@
 from pathlib import Path
+from subprocess import run, PIPE
 import os
 
 
 class Process:
-    def __init__(self):
-        pass
+    def __init__(self, process_path_or_name: str):
+        self.__process_path_or_name = process_path_or_name
 
-    @staticmethod
-    def kill_by_path(path: str):
-        cmd = f"""WMIC Process Where "ExecutablePath='{path}'" """
-        cmd += "Call Terminate"
+    def kill_by_path(self):
+        cmd = "WMIC Process Where "
+        cmd += f""""ExecutablePath='{self.__process_path_or_name}'" Call Terminate"""
         tmp_file = Path("tmp.bat")
         with tmp_file.open("w", encoding="utf-8") as f:
             f.write(cmd)
-        os.system(str(tmp_file))
+        run(cmd, stdout=PIPE, stderr=PIPE)
         os.remove(tmp_file)
+
+    def kill_by_name(self):
+        cmd = f"taskkill /f /im {self.__process_path_or_name}"
+        run(cmd, stdout=PIPE, stderr=PIPE)
