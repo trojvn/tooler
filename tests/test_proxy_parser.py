410ad44615add3596745ba480e2627980df7c53d
trojvn <mrx.trojan@gmail.com>
2024-12-02 23:09:44 +0300

    with socks5 tests

--- tests/test_proxy_parser.py
+++ tests/test_proxy_parser.py
@@ -49,3 +49,33 @@ def test_proxy_parser_from_url_without_creds():
     assert parser.port == 81
     assert parser.user is None
     assert parser.pswd is None
+
+
+def test_proxy_parser_socks5():
+    proxy = "socks5:1.1.1.1:80:user:pswd"
+    parser = ProxyParser(proxy)
+    assert parser.type == "http"
+    assert parser.ip == "1.1.1.1"
+    assert parser.port == 80
+    assert parser.user == "user"
+    assert parser.pswd == "pswd"
+
+
+def test_proxy_parser_from_url_socks5():
+    proxy = "socks5://user:pswd@1.1.1.1:80"
+    parser = ProxyParser(proxy)
+    assert parser.type == "http"
+    assert parser.ip == "1.1.1.1"
+    assert parser.port == 80
+    assert parser.user == "user"
+    assert parser.pswd == "pswd"
+
+
+def test_proxy_parser_from_url_without_creds_socks5():
+    proxy = "socks5://1.1.1.1:81"
+    parser = ProxyParser(proxy)
+    assert parser.type == "http"
+    assert parser.ip == "1.1.1.1"
+    assert parser.port == 81
+    assert parser.user is None
+    assert parser.pswd is None
